efe07325248186d5ee921ce938352ace4b3a807d
Sai priya <161415955+Saipriya-10@users.noreply.github.com>
2024-02-28 20:50:22 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int N = 3; 
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
+        }
+    }
+}
