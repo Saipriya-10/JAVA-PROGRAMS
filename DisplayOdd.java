541a220b63bc092b30fbe0ecfe4aacd40ebcbefa
Sai priya <161415955+Saipriya-10@users.noreply.github.com>
2024-02-28 20:48:02 +0530

    Add files via upload

---
+++ DisplayOdd.java
@@ -0,0 +1,16 @@
+public class DisplayOdd{
+    public static void main(String[] args) {
+        System.out.println("List of odd numbers:");
+        displayOddNumbers(1, 100);
+    }
+
+    private static void displayOddNumbers(int number, int end) {
+        if (number > end) return;
+        if (number % 2 != 0) {
+            System.out.print(number + " ");
+            displayOddNumbers(number + 2, end);
+        } else {
+            displayOddNumbers(number + 1, end);
+        }
+    }
+}
